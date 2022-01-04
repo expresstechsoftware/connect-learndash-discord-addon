f1ff665f6161e18f240b05f674c533db54603d26
younes-dro <dro66_8@hotmail.fr>
2022-01-04 05:10:25 +0100

    Set Default options values when the plugin gets activated

--- includes/class-learndash-discord-activator.php
+++ includes/class-learndash-discord-activator.php
@@ -30,7 +30,21 @@ class Learndash_Discord_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		update_option( 'ets_learndash_discord_send_welcome_dm', true );
+		update_option( 'ets_learndash_discord_welcome_message', 'Hi [LD_STUDENT_NAME] ([LD_STUDENT_EMAIL]), Welcome, Your courses [LD_COURSES],' );                
+		update_option( 'ets_learndash_discord_send_course_complete_dm', true );
+		update_option( 'ets_learndash_discord_course_complete_message', 'Hi [LD_STUDENT_NAME] ([LD_STUDENT_EMAIL]), You have completed the course  [LD_COURSE_NAME] at [LD_COURSE_COMPLETE_DATE]' );
+		update_option( 'ets_learndash_discord_send_lesson_complete_dm', true );
+		update_option( 'ets_learndash_discord_lesson_complete_message', 'Hi [LD_STUDENT_NAME] ([LD_STUDENT_EMAIL]), You have completed the lesson  [LD_LESSON_NAME] at [LD_COURSE_LESSON_DATE]' );                
+		update_option( 'ets_learndash_discord_send_topic_complete_dm', true );
+		update_option( 'ets_learndash_discord_topic_complete_message', 'Hi [LD_STUDENT_NAME] ([LD_STUDENT_EMAIL]), You have completed the topic  [LD_TOPIC_NAME] at [LD_COURSE_TOPIC_DATE]' );                 
+		update_option( 'ets_learndash_discord_retry_failed_api', true );
+		update_option( 'ets_learndash_discord_kick_upon_disconnect', false ); 
+		update_option( 'ets_learndash_discord_log_messages', false ); 
+		update_option( 'ets_learndash_discord_retry_api_count', 5 );
+		update_option( 'ets_learndash_discord_job_queue_concurrency', 1 );
+		update_option( 'ets_learndash_discord_job_queue_batch_size', 6 );
+		update_option( 'ets_learndash_discord_log_api_response', false );
 	}
 
 }
