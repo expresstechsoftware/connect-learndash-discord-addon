de09df6fc6e839c2aabbe184cdb5180ed8b2e64f
Younes DRO <dro66_8@hotmail.fr>
2023-04-21 02:39:32 +0000

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -13,16 +13,17 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+$ets_learndash_discord_data_erases = sanitize_text_field( trim( get_option( 'ets_learndash_discord_data_erases' ) ) );
 if ( defined( 'WP_UNINSTALL_PLUGIN' )
 		&& $_REQUEST['plugin'] === 'connect-learndash-discord-addon/learndash-discord.php'
 		&& $_REQUEST['slug'] === 'connect-learndash-and-discord'
 	&& wp_verify_nonce( $_REQUEST['_ajax_nonce'], 'updates' )
+	&& $ets_learndash_discord_data_erases
   ) {
-	$ets_learndash_discord_data_erases = sanitize_text_field( trim( get_option( 'ets_learndash_discord_data_erases' ) ) );
-	if ( $ets_learndash_discord_data_erases == true ) {
-		global $wpdb;
-		$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "usermeta WHERE `meta_key` LIKE '_ets_learndash_discord%'" );
-		$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "options WHERE `option_name` LIKE 'ets_learndash_discord_%'" );
-	}
+
+	global $wpdb;
+	$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "usermeta WHERE `meta_key` LIKE '_ets_learndash_discord%'" );
+	$wpdb->query( 'DELETE FROM ' . $wpdb->prefix . "options WHERE `option_name` LIKE 'ets_learndash_discord_%'" );
+
 }
 
