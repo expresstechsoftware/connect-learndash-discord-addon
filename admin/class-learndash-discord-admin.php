71457738f588ecec3c3ec163a74b7ec022e4fc06
Younes DRO <dro66_8@hotmail.fr>
2023-09-01 19:05:59 +0100

    Add review notification and update the add-on version to 1.0.10

--- admin/class-learndash-discord-admin.php
+++ admin/class-learndash-discord-admin.php
@@ -839,4 +839,31 @@ class Learndash_Discord_Admin {
 			}
 		}
 	}
+
+	/**
+	 *
+	 * Update user meta notification
+	 */
+	public function ets_learndash_discord_notice_dismiss() {
+
+		if ( ! is_user_logged_in() ) {
+			wp_send_json_error( 'Unauthorized user', 401 );
+			exit();
+		}
+
+		// Check for nonce security
+		if ( ! wp_verify_nonce( $_POST['ets_learndash_discord_nonce'], 'ets-learndash-discord-ajax-nonce' ) ) {
+				wp_send_json_error( 'You do not have sufficient rights', 403 );
+				exit();
+		}
+
+		update_user_meta( get_current_user_id(), '_ets_learndash_discord_dismissed_notification', true );
+		$event_res = array(
+			'status'  => 1,
+			'message' => __( 'success', 'connect-learndash-and-discord' ),
+		);
+		return wp_send_json( $event_res );
+
+		exit();
+	}
 }
