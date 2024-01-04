3355a8116de75ca2187dafd7cf9a7f0e12169e69
Younes DRO <dro66_8@hotmail.fr>
2024-01-04 10:54:59 +0100

    Added documentation links for enhanced user guidance.

--- admin/partials/pages/learndash_discord_documentation.php
+++ admin/partials/pages/learndash_discord_documentation.php
@@ -1,22 +1,22 @@
 <div class="ets-tutor">
 	<ul>
 		<li>
+			<label><?php esc_html_e( 'Step-by-Step Written guide on how to connect the plugin with Discord server', 'connect-learndash-and-discord' ); ?>
+			</label>
+			<a href="https://www.expresstechsoftwares.com/step-by-step-documentation-guide-on-how-to-connect-learndash-and-discord-server/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-learndash-and-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
+		</li>		
+		<li>
 			<label><?php esc_html_e( 'YouTube video tutorial on how to connect the plugin with discord server', 'connect-learndash-and-discord' ); ?>
 			</label>
 			<a href="https://youtu.be/v7lxB_Bvlv4" target="_blank" ><?php esc_html_e( 'Click Here' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 		<li>
-			 <label><?php esc_html_e( 'Step-by-Step Written guide on how to connect the plugin with Discord server', 'connect-learndash-and-discord' ); ?>
-			</label>
-			<a href="https://github.com/ravisoni6262/discord-addons-documentation" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-learndash-and-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
-		</li>
-		<li>
 			<label><?php esc_html_e( 'Features and other documentation', 'connect-learndash-and-discord' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/pmpro-official-discord-add-on" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-learndash-and-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/discord-add-ons-documentation/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-learndash-and-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
 		<li>
-			<b>Opt-out : </b><label><?php esc_html_e( 'By checking the box "Data erases on uninstall?" in advanced settings Tab , you are indicating that you want to delete all data associated with the plugin when it is uninstalled. This includes any settings, preferences, or other data that the plugin may have saved on your system. Please note that this action cannot be undone, so be sure that you want to delete the data before uninstalling the plugin.', 'connect-learndash-and-discord' ); ?></label>
+			<b><?php esc_html_e( 'Opt-out :', 'connect-learndash-and-discord' ); ?> </b><label><?php esc_html_e( 'By checking the box "Data erases on uninstall?" in advanced settings Tab , you are indicating that you want to delete all data associated with the plugin when it is uninstalled. This includes any settings, preferences, or other data that the plugin may have saved on your system. Please note that this action cannot be undone, so be sure that you want to delete the data before uninstalling the plugin.', 'connect-learndash-and-discord' ); ?></label>
 		</li>
 	</ul>
 </div>
